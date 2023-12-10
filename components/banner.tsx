ca3c1e7321d8dbb75df2625b0b9757d143c8cd4a
Shahadat Hussain <shahadat.rockstar@gmail.com>
2023-12-10 16:40:21 +0530

    Tech update

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,22 +1,114 @@
 import { Scene } from "react-scrollmagic-r18";
+import { FaReact, FaSass, FaHtml5, FaCss3Alt, FaBootstrap, FaWordpress, FaGithub } from "react-icons/fa";
+import { SiJavascript, SiTailwindcss, SiAzuredevops, SiJquery } from "react-icons/si";
+// import { VscJson } from "react-icons/Vsc";
+import { TbApi, TbBrandNextjs } from "react-icons/tb";
+// import { DiJqueryLogo, DiGit, DiPhotoshop } from "react-icons/Di";
+const techLogos = [
+  {
+    name: "React",
+    imageUrl: "/images/logos/reactjs.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-sky-400 icon-react",
+    icon: FaReact,
+  },
+  {
+    name: "NextJS",
+    imageUrl: "/images/logos/nextjs.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-gray-100 icon-nextjs",
+    icon: TbBrandNextjs,
+  },
+  {
+    name: "HTML5",
+    imageUrl: "/images/logos/sass.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-orange-600 icon-html",
+    icon: FaHtml5,
+  },
+  {
+    name: "CSS3",
+    imageUrl: "/images/logos/sass.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-blue-400 icon-css",
+    icon: FaCss3Alt,
+  },
+  {
+    name: "JavaScript",
+    imageUrl: "/images/logos/javascript.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-5 w-5 text-yellow-300 icon-javascript",
+    icon: SiJavascript,
+  },
 
+  {
+    name: "SCSS/SASS",
+    imageUrl: "/images/logos/sass.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-pink-500 icon-scss-sass",
+    icon: FaSass,
+  },
+  {
+    name: "TailwindCSS",
+    imageUrl: "/images/logos/tailwind.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-sky-500 icon-tailwindcss",
+    icon: SiTailwindcss,
+  },
+  {
+    name: "Bootstrap",
+    imageUrl: "/images/logos/bootstrap.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-violet-600 icon-bootstrap",
+    icon: FaBootstrap,
+  },
+  {
+    name: "GitHub",
+    imageUrl: "/images/logos/github.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-6 w-6 text-gray-100 icon-github",
+    icon: FaGithub,
+  },
+  {
+    name: "Azure DevOps",
+    imageUrl: "/images/logos/azure.svg",
+    iconWidth: "h-6 w-6",
+    brandsColor: "h-5 w-5 text-blue-500 icon-azure-devops",
+    icon: SiAzuredevops,
+  },
+];
 const Banner = () => {
-    return (
-        <section id="home" className="hero hero--dark">
-            <div className="wrapper">
-                <div className="wrapper--push">
-                    <div className="wrapper--content">
-                        <span className='tag-line'>Hi, my name is</span>
-                        <h1 className="main-headline">Shahadat Hussain.</h1>
-                        {/* <h1 className="main-headline">What was Impossible Technology makes Possible.</h1> */}
-                        <h2 className="sub-headline">I&apos;m a front-end developer.</h2>
-                        <h3 className="hero-content">I build interactive website that run across <span>platforms &amp; devices.</span></h3>
-                        <a className="scroll-mouse" href="#about" aria-label="UILab.in"><span>Scroll Mouse</span></a>
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="home" className="hero hero--dark">
+      <div className="wrapper">
+        <div className="wrapper--push">
+          <div className="wrapper--content">
+            <span className="tag-line">Hi, my name is</span>
+            <h1 className="main-headline">Shahadat Hussain.</h1>
+            {/* <h1 className="main-headline">What was Impossible Technology makes Possible.</h1> */}
+            <h2 className="sub-headline">I&apos;m a front-end developer.</h2>
+            <h3 className="hero-content">
+              I build interactive website that run across <span>platforms &amp; devices.</span>
+            </h3>
 
-export default Banner
+            <div className="technologies">
+              <ul>
+                {techLogos.map((index) => (
+                  <li>
+                    <index.icon className={"flex-shrink-0 " + index.brandsColor} aria-hidden="true" />
+                    <span className="tech-text">{index.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <a className="scroll-mouse" href="#about" aria-label="UILab.in">
+              <span>Scroll Mouse</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
--- pages/api/portfolioAPI.ts
+++ pages/api/portfolioAPI.ts
@@ -1,17 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 const data = [
-{
-    title: "ResumeBuild",
-    description: "ReactJS, Redux, REST API, JSON, HTML5, SCSS/SASS, React-Bootstrap, Git and Azure DevOps.",
-    href: "https://resumebuild.com/app/introduction",
-    imgJPG: "/images/port-resume.jpg",
-    imgWEBP: "/images/webp/port-resume.webp",
-    imgAVIF: "/images/avif/port-resume.avif",
+  {
+    title: "Dashboard - WorkSimpli Products",
+    description: "ReactJS, Redux, REST API, JSON, HTML5, TailwindCSS, TailwindUI, Git and Azure DevOps.",
+    href: "https://signsimpli.com/app/dashboard/",
+    imgJPG: "/images/port-dashboard.jpg",
+    imgWEBP: "/images/webp/port-dashboard.webp",
+    imgAVIF: "/images/avif/port-dashboard.avif",
     btnTitle: "ResumeBuild.com",
-},
-{
+  },
+  {
     title: "Quint Digital Media Limited",
     description: "ReactJS, Grids susy grids, HTML5, SCSS/SASS, JSON, JavaScript, CSS Animations Git and GitHub.",
     href: "https://www.thequint.com/",
@@ -19,8 +19,8 @@ const data = [
     imgWEBP: "/images/webp/port-2.webp",
     imgAVIF: "/images/avif/port-2.avif",
     btnTitle: "TheQuint.com",
-},
-{
+  },
+  {
     title: "UILab.in",
     description: "ReactJS, NextJS, JSON, TailwindCSS, HTML5, JavaScript, CSS Animations and GitHub.",
     href: "https://www.uilab.in/",
@@ -28,8 +28,8 @@ const data = [
     imgWEBP: "/images/webp/port-13.webp",
     imgAVIF: "/images/avif/port-13.avif",
     btnTitle: "UILab.in",
-},
-{
+  },
+  {
     title: "Chat App for Fun - The Quint",
     description: "HTML5, CSS3, BEM, JavaScript, jQuery, JSON, CSS Animations, Git and GitHub.",
     href: "https://www.thequint.com/the-quint-lab/mahatma-gandhi-jayanti-celebrations-2nd-october-chat-app",
@@ -37,53 +37,58 @@ const data = [
     imgWEBP: "/images/webp/port-chat.webp",
     imgAVIF: "/images/avif/port-chat.avif",
     btnTitle: "TheQuint.com",
-},
-{
+  },
+  {
     title: "PDFSimpli.com",
-    description: "Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.",
+    description:
+      "Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.",
     href: "https://pdfsimpli.com/",
     imgJPG: "/images/port-pdf.jpg",
     imgWEBP: "/images/webp/port-pdf.webp",
     imgAVIF: "/images/avif/port-pdf.avif",
     btnTitle: "PDFSimpli.com",
-},
-{
+  },
+  {
     title: "SignSimpli.com",
-    description: "Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.",
+    description:
+      "Created component-based layout, ASP.NET MVC framework, Bootstrap, CSS pre-processor SCSS/SASS and Azure.",
     href: "https://signsimpli.com/",
     imgJPG: "/images/port-12.jpg",
     imgWEBP: "/images/webp/port-12.webp",
     imgAVIF: "/images/avif/port-12.avif",
     btnTitle: "SignSimpli.com",
-},
-{
+  },
+  {
     title: "Care4Life",
-    description: "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.",
+    description:
+      "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.",
     href: "https://care4life.com/",
     imgJPG: "/images/port-3.jpg",
     imgWEBP: "/images/webp/port-3.webp",
     imgAVIF: "/images/avif/port-3.avif",
     btnTitle: "Care4Life.com",
-},
-{
+  },
+  {
     title: "Text4Baby.org",
-    description: "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.",
+    description:
+      "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slick Slider.",
     href: "https://text4baby.org/",
     imgJPG: "/images/port-4.jpg",
     imgWEBP: "/images/webp/port-4.webp",
     imgAVIF: "/images/avif/port-4.avif",
     btnTitle: "Text4Baby.org",
-},
-{
+  },
+  {
     title: "Beyond the Orphanage",
-    description: "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Swiper Slider.",
+    description:
+      "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Swiper Slider.",
     href: "https://beyondtheorphanage.org/",
     imgJPG: "/images/port-11.jpg",
     imgWEBP: "/images/webp/port-11.webp",
     imgAVIF: "/images/avif/port-11.avif",
     btnTitle: "BeyondTheOrphanage.org",
-},
-{
+  },
+  {
     title: "Buri Ladki - The Quint",
     description: "Who is a 'Buri Ladki'? You decide. Developed interactive game with full screen slider.",
     href: "https://www.thequint.com/quintlab/buri-ladki-game/",
@@ -91,8 +96,8 @@ const data = [
     imgWEBP: "/images/webp/port-5.webp",
     imgAVIF: "/images/avif/port-5.avif",
     btnTitle: "BuriLadkiGame",
-},
-{
+  },
+  {
     title: "Braahmam Net Solutions Pvt Ltd",
     description: "Converted PSD to WordPress, theme customization, manage widget and plugins.",
     href: "https://braahmam.net/",
@@ -100,8 +105,8 @@ const data = [
     imgWEBP: "/images/webp/port-6.webp",
     imgAVIF: "/images/avif/port-6.avif",
     btnTitle: "braahmam.net",
-},
-{
+  },
+  {
     title: "Motorola Company Partner - The Quint",
     description: "Developed a full screen parallax microsite for Phone-Life Balance.",
     href: "https://www.thequint.com/quintlab/phone-life-balance/",
@@ -109,8 +114,8 @@ const data = [
     imgWEBP: "/images/webp/port-8.webp",
     imgAVIF: "/images/avif/port-8.avif",
     btnTitle: "PhoneLifeBalance",
-},
-{
+  },
+  {
     title: "Concept of Consent - The Quint",
     description: "Custom Animated slider with transition effect quiz.",
     href: "https://www.thequint.com/quintlab/concept-of-consent/",
@@ -118,17 +123,18 @@ const data = [
     imgWEBP: "/images/webp/port-9.webp",
     imgAVIF: "/images/avif/port-9.avif",
     btnTitle: "Concept Of Concent",
-},
-{
+  },
+  {
     title: "WebCircle.com.au",
-    description: "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slider.",
+    description:
+      "Developed mobile-first responsive code with latest technology HTML5, CSS3, Bootstrap, JavaScript and Slider.",
     href: "https://webcircle.com.au/",
     imgJPG: "/images/port-10.jpg",
     imgWEBP: "/images/webp/port-10.webp",
     imgAVIF: "/images/avif/port-10.avif",
     btnTitle: "WebCircle.com.au",
-},
-{
+  },
+  {
     title: "Sandesh to a Soldier - The Quint",
     description: "Developed a full screen parallax microsite to send a message to the Soldier.",
     href: "https://www.thequint.com/quintlab/sandesh-to-a-soldier/",
@@ -136,8 +142,8 @@ const data = [
     imgWEBP: "/images/webp/port-7.webp",
     imgAVIF: "/images/avif/port-7.avif",
     btnTitle: "Sandesh to a Soldier",
-},
-{
+  },
+  {
     title: "Jal Mahotsav - The Quint",
     description: "Developed a full screen parallax microsite for Jal Mahotsav is the ultimate watercation.",
     href: "https://www.thequint.com/quintlab/jal-mahotsav/",
@@ -145,8 +151,8 @@ const data = [
     imgWEBP: "/images/webp/port-jal-mahotsav.webp",
     imgAVIF: "/images/avif/pport-jal-mahotsav.avif",
     btnTitle: "PhoneLifeBalance",
-},
-{
+  },
+  {
     title: "About 2.0 Movie - AMP Story",
     description: "Develop component-based layout,  Card based layout, Accelerated Mobile Pages (AMP).",
     href: "https://www.thequint.com/quintlab/ampstories/2-point-0-movie-interesting-facts/",
@@ -154,8 +160,8 @@ const data = [
     imgWEBP: "/images/webp/port-amp-1.webp",
     imgAVIF: "/images/avif/port-amp-1.avif",
     btnTitle: "About 2.0 Movie - AMP Story",
-},
-{
+  },
+  {
     title: "Sanjay Dutt Life - AMP Story",
     description: "Develop component-based layout,  Card based layout, Accelerated Mobile Pages (AMP).",
     href: "https://www.thequint.com/quintlab/what-sanju-missed-about-sanjay-dutts-life/",
@@ -163,13 +169,8 @@ const data = [
     imgWEBP: "/images/webp/port-amp-2.webp",
     imgAVIF: "/images/avif/port-amp-2.avif",
     btnTitle: "Sanjay Dutt Life - AMP Story",
-}
-]
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json(
-    data
-  )
+  },
+];
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json(data);
 }
