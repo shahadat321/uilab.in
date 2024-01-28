9c7c780b9b72ca6383fa153c834f5a843333eeb5
Shahadat Hussain <shahadat.rockstar@gmail.com>
2024-01-28 21:19:14 +0530

    UNDO Cloud images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["uilab-images.s3.eu-north-1.amazonaws.com"],
-  },
+  // images: {
+  //   domains: ["uilab-images.s3.eu-north-1.amazonaws.com"],
+  // },
 };
 
 module.exports = nextConfig;
